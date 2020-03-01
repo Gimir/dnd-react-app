f0fa49ea43f2f31349a7c18d1d8570a7dd02aa0d
Gimir <mukhammad.amaev@mail.ru>
2020-03-01 21:21:17 +0300

    Add rows action creators

---
+++ src/actions/rows.js
@@ -0,0 +1,115 @@
+import axios from 'axios';
+import { UPDATE_CARD, ADD_CARD, REMOVE_CARD, SET_CARDS } from '../constants/actionTypes';
+import routes from '../constants/routes';
+
+//SYNC ACTION CREATORS
+const setCardsSync = cards => ({
+  type: SET_CARDS,
+  payload: cards,
+});
+
+const addCardSync = card => ({
+  type: ADD_CARD,
+  payload: card,
+});
+const updateCardSync = (card, source, destination) => ({
+  type: UPDATE_CARD,
+  payload: {
+    card,
+    source,
+    destination,
+  },
+});
+const removeCardSync = (rowId, cardId) => ({
+  type: REMOVE_CARD,
+  payload: {
+    rowId,
+    cardId,
+  },
+});
+
+//ASYNC ACTION CREATORS
+export const setCards = () => {
+  return (dispatch, getState) => {
+    const { token } = getState().user;
+
+    axios({
+      method: 'get',
+      url: routes.get_cards,
+      headers: {
+        Authorization: `JWT ${token}`,
+      },
+    })
+      .then(responce => dispatch(setCardsSync(responce.data)))
+      .catch(err => console.log(err));
+  };
+};
+
+export const addCard = (rowId, text) => {
+  return (dispatch, getState) => {
+    const { token } = getState().user;
+
+    axios({
+      method: 'post',
+      url: routes.add_card,
+      data: {
+        row: String(rowId),
+        text: text,
+      },
+      contentType: 'application/json',
+      dataType: 'json',
+      headers: {
+        Authorization: `JWT ${token}`,
+      },
+    })
+      .then(responce => dispatch(addCardSync(responce.data)))
+      .catch(err => console.log(err));
+  };
+};
+
+export const updateCard = (cardId, source, destination) => {
+  return (dispatch, getState) => {
+    const { token } = getState().user;
+    const row = getState().rows.find(row => String(row.id) === source.droppableId);
+    const card = row.cards.find(card => String(card.id) === cardId);
+    card.seq_num = destination.index;
+    dispatch(updateCardSync(card, source, destination));
+    axios({
+      method: 'patch',
+      url: routes.update_card(card.id),
+      data: {
+        row: destination.droppableId,
+        seq_num: destination.index,
+        text: card.text,
+      },
+      contentType: 'application/json',
+      headers: {
+        Authorization: `JWT ${token}`,
+      },
+    })
+      .then(() => console.log('Card reordered!'))
+      .catch(err => {
+        console.log(err);
+        alert('Your chainges unsaved!');
+      });
+  };
+};
+
+export const removeCard = (rowId, cardId) => {
+  return (dispatch, getState) => {
+    const { token } = getState().user;
+    dispatch(removeCardSync(rowId, cardId));
+    axios({
+      method: 'delete',
+      url: routes.delete_card(cardId),
+      headers: {
+        Authorization: `JWT ${token}`,
+      },
+    })
+      .then(() => console.log('Card deleted!'))
+      .catch(err => {
+        alert('Your chainges unsaved!');
+        console.log(err);
+      });
+  };
+};
