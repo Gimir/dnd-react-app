af8cd12b1ce9b75d1f8a08f51e02b1124ea3fc53
Gimir <mukhammad.amaev@mail.ru>
2020-03-01 21:19:36 +0300

    Add action types

---
+++ src/constants/actionTypes.js
@@ -0,0 +1,5 @@
+export const UPDATE_CARD = 'UPDATE_CARD';
+export const ADD_CARD = 'ADD_CARD';
+export const REMOVE_CARD = 'REMOVE_CARD';
+export const LOG_IN = 'LOG_IN';
+export const SET_CARDS = 'SET_CARDS';
